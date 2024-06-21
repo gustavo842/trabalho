20f3604e96e81e83be53d4f01859453274e01c33
gustavo842 <00001106340607sp@al.educacao.sp.gov.br>
2024-06-21 20:13:13 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let XBolinha = 300;
+let ybolinha= 200;
+let diametro = 15;
+
+let velocidadeXBolinha = 6;
+let velocidadeybolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+function draw() {
+  background(0);
+  circle(XBolinha, ybolinha, diametro);
+  XBolinha += velocidadeXBolinha;
+  //yBolinha += velocidadeYBolinha;
+  
+  if (XBolinha > width ){
+                                       
+  }
+  }
+  
